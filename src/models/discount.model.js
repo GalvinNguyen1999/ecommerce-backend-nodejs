334e92257b37204571605077d62087c6bd80627f
Galvin <galvinnguyen123@gmail.com>
2026-08-11 23:27:28 +0700

    Section 20: Discount Model in eCommerce

---
+++ src/models/discount.model.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const { Schema, model } = require("mongoose");
+
+const DOCUMENT_NAME = "Discount";
+const COLLECTION_NAME = "Discounts";
+
+const discountSchema = new Schema(
+  {
+    discount_name: {
+      type: String,
+      required: true,
+    },
+    discount_description: {
+      type: String,
+      required: true,
+    },
+    discount_type: {
+      type: String,
+      default: "fixed_amount", // percentage
+    },
+    discount_value: {
+      type: String,
+      reuqired: true,
+    },
+    discount_code: {
+      type: String,
+      required: true,
+    },
+    discount_start_date: {
+      type: Date,
+      required: true,
+    },
+    discount_end_date: {
+      type: Date,
+      required: true,
+    },
+    discount_max_users: {
+      type: Number,
+      required: true,
+    },
+    discount_uses_count: {
+      type: Number,
+      reuqired: true,
+    },
+    discount_users_used: {
+      type: Array,
+      default: [],
+    },
+    discount_min_order_value: {
+      type: Number,
+      required: true,
+    },
+    discount_shopId: {
+      type: Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    discount_is_active: {
+      type: Boolean,
+      default: true,
+    },
+    discount_applies_to: {
+      type: String,
+      required: true,
+      enum: ["all", "specific"],
+    },
+    discount_product_ids: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  },
+);
+
+module.exports = model(DOCUMENT_NAME, discountSchema);
